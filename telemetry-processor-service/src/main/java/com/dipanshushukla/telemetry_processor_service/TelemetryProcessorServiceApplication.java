efdd7128c8f15dba372bcd63a3ddee9febd411de
Dipanshu Shukla <shuklascientist5@gmail.com>
2026-06-06 03:15:07 +0530

    feat: int telemetry processor service

---
+++ telemetry-processor-service/src/main/java/com/dipanshushukla/telemetry_processor_service/TelemetryProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.telemetry_processor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemetryProcessorServiceApplication.class, args);
+	}
+
+}
---
+++ telemetry-processor-service/src/test/java/com/dipanshushukla/telemetry_processor_service/TelemetryProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.telemetry_processor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryProcessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
