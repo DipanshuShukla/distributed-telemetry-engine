a87440bfff7685661801bc2d994f72b0d631ba3e
Dipanshu Shukla <shuklascientist5@gmail.com>
2026-06-06 03:14:19 +0530

    feat: init telemetry generator service

---
+++ telemetry-generator-service/src/main/java/com/dipanshushukla/telemetry_generator_service/TelemetryGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.telemetry_generator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryGeneratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemetryGeneratorServiceApplication.class, args);
+	}
+
+}
---
+++ telemetry-generator-service/src/test/java/com/dipanshushukla/telemetry_generator_service/TelemetryGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.telemetry_generator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryGeneratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
